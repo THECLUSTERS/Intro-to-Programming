ec36ff3209cb687890091db1b826fa0725851773
THECLUSTERS <SHUBHAKT025@GMAIL.COM>
2023-01-05 15:29:47 +0530

    Added the sum.cpp file

---
+++ sum.cpp
@@ -0,0 +1,21 @@
+// C++ program to add two numbers
+#include <bits/stdc++.h>
+
+int main()
+{
+	int A, B, sum = 0;
+
+	// Ask user to enter the two numbers
+	printf("Enter two numbers A and B : \n");
+
+	// Read two numbers
+	cin>>a>>b;
+
+	// Calculate the addition of A and B
+	 sum = A + B;
+
+	// Print the sum
+	cout<<"Sum of A and B is: "<<sum;
+
+	return 0;
+}
